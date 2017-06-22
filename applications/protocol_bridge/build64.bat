41e189e0e353c14e05246467c90e82a20cd66901
Michal Konopa <michal.konopa@microrisc.com>
2017-06-22 09:19:36 +0200

    Added INITIAL versions of build scripts. There is a problem with compilation with paho library - it will be corrected.

---
+++ applications/protocol_bridge/build64.bat
@@ -0,0 +1,48 @@
+set project=protocol_bridge
+
+rem //expected build dir structure
+set buildexp=build\\Visual_Studio_14_2015\\x64
+
+set currentdir=%cd%
+set builddir=.\\%buildexp%
+set libsdir=..\\..\\..
+
+mkdir %builddir%
+
+rem //get path to clibcdc libs
+set clibcdc=%libsdir%\\clibcdc\\%buildexp%
+pushd %clibcdc%
+set clibcdc=%cd%
+popd
+
+rem //get path to clibspi libs
+set clibspi=%libsdir%\\clibspi\\%buildexp%
+pushd %clibspi%
+set clibspi=%cd%
+popd
+
+rem //get path to clibdpa libs
+set clibdpa=%libsdir%\\clibdpa\\%buildexp%
+pushd %clibdpa%
+set clibdpa=%cd%
+popd
+
+rem //get path to cutils libs
+set cutils=%libsdir%\\cutils\\%buildexp%
+pushd %cutils%
+set cutils=%cd%
+popd
+
+rem //get path to iqrfd libs
+set iqrfd=..\\..\\daemon\\%buildexp%
+pushd %iqrfd%
+set iqrfd=%cd%
+popd
+
+rem //launch cmake to generate build environment
+pushd %builddir%
+cmake -G "Visual Studio 14 2015 Win64" -Dclibdpa_DIR:PATH=%clibdpa% -Dcutils_DIR:PATH=%cutils% -Dclibcdc_DIR:PATH=%clibcdc% -Dclibspi_DIR:PATH=%clibspi% -Diqrfd_DIR:PATH=%iqrfd% %currentdir%
+popd
+
+rem //build from generated build environment
+cmake --build %builddir%
---
+++ applications/protocol_bridge/buildMake.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Script for building IQRF startup application on Linux machine
+
+set -e
+
+project=protocol_bridge
+
+#expected build dir structure
+buildexp=build/Unix_Makefiles
+
+currentdir=$PWD
+builddir=./${buildexp}
+LIB_DIRECTORY=${1:-../../..}
+
+mkdir -p ${builddir}
+
+#get path to clibcdc libs
+clibcdc=${LIB_DIRECTORY}/clibcdc/${buildexp}
+pushd ${clibcdc}
+clibcdc=$PWD
+popd
+
+#get path to clibspi libs
+clibspi=${LIB_DIRECTORY}/clibspi/${buildexp}
+pushd ${clibspi}
+clibspi=$PWD
+popd
+
+#get path to clibdpa libs
+clibdpa=${LIB_DIRECTORY}/clibdpa/${buildexp}
+pushd ${clibdpa}
+clibdpa=$PWD
+popd
+
+#get path to cutils libs
+cutils=${LIB_DIRECTORY}/cutils/${buildexp}
+pushd ${cutils}
+cutils=$PWD
+popd
+
+#get path to iqrfd libs
+iqrfd=../../daemon/${buildexp}
+pushd ${iqrfd}
+iqrfd=${PWD}
+popd
+
+#launch cmake to generate build environment
+pushd ${builddir}
+pwd
+cmake -G "Unix Makefiles" -Dclibcdc_DIR:PATH=${clibcdc} -Dclibspi_DIR:PATH=${clibspi} -Dclibdpa_DIR:PATH=${clibdpa} -Dcutils_DIR:PATH=${cutils} -Diqrfd_DIR:PATH=${iqrfd} ${currentdir} -DCMAKE_BUILD_TYPE=Debug
+popd
+
+#build from generated build environment
+cmake --build ${builddir}
