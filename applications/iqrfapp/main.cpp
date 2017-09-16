fa62bdbd00b5810d2ed307acd94e2b9e8d317376
Frantisek Mikulu <frantisek.mikulu@iqrf.org>
2017-09-16 17:09:49 +0200

    Iqrfapp redesign

--- applications/iqrfapp/main.cpp
+++ applications/iqrfapp/main.cpp
@@ -14,257 +14,510 @@
  * limitations under the License.
  */
 
+#include "TaskQueue.h"
 #include "MqChannel.h"
 #include "PlatformDep.h"
 #include "IqrfLogging.h"
 #include "JsonUtils.h"
 #include "VersionInfo.h"
+#include <string>
 #include <fstream>
+#include <stdexcept>
+
+class Iqrfapp
+{
+public:
+  Iqrfapp();
+  ~Iqrfapp();
+  int run(const std::vector<std::string>& params);
+
+private:
+  const std::string CMD_HELP = "help";
+  const std::string CMD_READO = "readonly";
+  const std::string CMD_READA = "readall";
+  const std::string CMD_RAW = "raw";
+  const std::string CMD_CONF = "conf";
+  const std::string CMD_JSON = "json";
+  const std::string CMD_EXIT = "exit";
+
+  int runCmd(const std::vector<std::string>& params);
+  void handleMessageReceived(const ustring& message);
+  bool waitForMessageReceived(int extraTime = 0);
+  void showWaitForMessageReceivedResult(bool res);
+  void setTimeout(int timeout);
+  void sendParamsAsMessage(const std::vector<std::string>& params);
+
+  void interactiveCmd();
+  void helpCmd();
+  void readonlyCmd(const std::vector<std::string>& params);
+  void readallCmd(const std::vector<std::string>& params);
+  void rawCmd(const std::vector<std::string>& params);
+  void confCmd(const std::vector<std::string>& params);
+  void jsonCmd(const std::vector<std::string>& params);
+
+  std::string m_cfgFileName;
+  std::string m_localMqName;
+  std::string m_remoteMqName;
+  int m_defaultTimeout = 5000;
+
+  TaskQueue<ustring> *m_msgQueue = nullptr;
+  MqChannel* m_mqChannel = nullptr;
+
+  std::mutex m_mutexMessageReceived;
+  std::condition_variable m_conditionMessageReceived;
+  bool m_flagMessageReceived = false;
+  ustring m_messageReceived;
+  int m_actualTimeout = 5000;
+
+  bool exitFlag = false;
+  bool sentFlag = false;
+};
 
 TRC_INIT()
 
-bool exitFlag = false;
-bool sentFlag = false;
-
-void helpAndExit()
-{
-  std::cerr << "Usage" << std::endl;
-  std::cerr << "  iqrfapp" << std::endl;
-  std::cerr << "  iqrfapp help" << std::endl;
-  std::cerr << "  iqrfapp readonly" << std::endl;
-  std::cerr << "  iqrfapp raw <DpaBytesSeparatedByDots> timeout <ValueInMs>" << std::endl;
-  std::cerr << "  iqrfapp conf [operational|forwarding|service]" << std::endl;
-  std::cerr << "  iqrfapp <JsonDpaRequest>" << std::endl;
-  std::cerr << "Examples" << std::endl;
-  std::cerr << "  iqrfapp" << std::endl;
-  std::cerr << "  iqrfapp help" << std::endl;
-  std::cerr << "  iqrfapp raw 01.00.06.03.ff.ff timeout 1000" << std::endl;
-  std::cerr << "  iqrfapp conf operational" << std::endl;
-  std::cerr << "  iqrfapp \"{\\\"ctype\\\":\\\"dpa\\\",\\\"type\\\":\\\"raw\\\",\\\"timeout\\\":1000,\\\"request\\\":\\\"01.00.06.03.ff.ff\\\"}\"" << std::endl;
-                  /*"{\"ctype\":\"dpa\",\"type\":\"raw\",\"timeout\":1000,\"request\":\"01.00.06.03.ff.ff\"}"*/
-  std::cerr << "Tips" << std::endl;
-  std::cerr << "  Adjust default timeout in iqrfapp.json to the needs of the application using iqrfapp tool or define timeout as cmd param." << std::endl;
-  std::cerr << "  Default timeout in iqrfapp.json is 5000 ms." << std::endl;
-  exit(-1);
-}
-
-int parseCommand(std::string cmd) {
-
-  rapidjson::Document doc;
-  int timeout = -1;
-
-  try {
-    jutils::parseString(cmd, doc);
-    jutils::assertIsObject("", doc);
-    timeout = jutils::getMemberAs<int>("timeout", doc);
-  }
-  catch (std::exception &e) {
-    CATCH_EX("Cannot parse command: ", std::logic_error, e);
-  }
-
-  return timeout;
-}
-
-int handleMessageFromMq(const ustring& message)
-{
-  TRC_DBG("Received from MQ: " << std::endl << FORM_HEX(message.data(), message.size()));
-  std::string msg((char*)message.data(), message.size());
-
-  if (sentFlag) {
-    std::cout << msg << std::endl;
-    exitFlag = true;
-    sentFlag = false;
-  }
-
-  return 0;
-}
-
-//just workaround - TODO cmdl params are not properly tested - maybe boost?
+//TODO cmdl params are not properly parsed in expected cmdl tools way - maybe boost?
 int main(int argc, char** argv)
 {
   TRC_START("", iqrf::Level::inf, 0);
-  std::string command;
-  std::string rawTimeout;
 
-  bool cmdl = false;
-  bool json = false;
-  bool raw = false;
-  bool readonly = false;
+  TRC_ENTER("");
 
-  int defaultTimeout = 5000;
+  Iqrfapp iqrfapp;
 
-  switch (argc) {
-  case 1:
-  {
-    cmdl = true;
-    break;
-  }
-  case 2:
-  {
-    std::string arg1 = argv[1];
-    if (arg1 == "help") {
-      helpAndExit();
-    }
-    else if (arg1 == "readonly") {
-      readonly = true;
-    }
-    else {
-      json = true;
-    }
-    break;
-  }
-  case 3:
-  {
-    std::string arg1 = argv[1];
-    std::string arg2 = argv[2];
-    if (arg1 == "conf" && arg2 != "operational" && arg2 != "forwarding" && arg2 != "service") {
-      helpAndExit();
-    }
-    if (arg1 != "raw") {
-      helpAndExit();
-    }
-    break;
-  }
-  // raw with timeout
-  case 5:
-  {
-    std::string arg1 = argv[1];
-    std::string arg2 = argv[2];
-    std::string arg3 = argv[3];
-    std::string arg4 = argv[4];
+  std::vector<std::string> params;
 
-    if (arg1 != "raw" && arg3 != "timeout") {
-      helpAndExit();
-    }
-    else {
-      raw = true;
-      rawTimeout = arg4;
-    }
-    break;
-  }
-  default:; //just continue and try
-  }
-
-  std::ostringstream os;
   for (int i = 1; i < argc; i++)
-    os << argv[i] << " ";
-  command = os.str();
+    params.push_back(argv[i]);
 
-  std::string cfgFileName("iqrfapp.json");
-  std::string localMqName("iqrf-daemon-100");
-  std::string remoteMqName("iqrf-daemon-110");
+  int retval = iqrfapp.run(params);
+
+  TRC_LEAVE(PAR(retval));
+  return retval;
+}
+
+Iqrfapp::Iqrfapp()
+{
+  TRC_ENTER("");
+
+  std::string m_cfgFileName = "iqrfapp.json";
+  std::string localMqName = "iqrf-daemon-100";
+  std::string remoteMqName = "iqrf-daemon-110";
+
   std::ifstream f;
 
-  f.open(cfgFileName);
+  //open in working directory
+  f.open(m_cfgFileName);
 
   if (!f.is_open()) {
     std::string pth = "/etc/iqrf-daemon/";
-    cfgFileName = pth + cfgFileName;
-    f.open(cfgFileName);
+    m_cfgFileName = pth + m_cfgFileName;
+    
+    //open in default path
+    f.open(m_cfgFileName);
   }
 
   if (f.is_open()) {
-    TRC_DBG("Reading cfg from: " << PAR(cfgFileName));
+    TRC_DBG("Reading cfg from: " << PAR(m_cfgFileName));
 
     rapidjson::Document cfg;
     try {
-      jutils::parseJsonFile(cfgFileName, cfg);
+      jutils::parseJsonFile(m_cfgFileName, cfg);
       jutils::assertIsObject("", cfg);
 
-      localMqName = jutils::getPossibleMemberAs<std::string>("LocalMqName", cfg, localMqName);
-      remoteMqName = jutils::getPossibleMemberAs<std::string>("RemoteMqName", cfg, remoteMqName);
+      m_localMqName = jutils::getPossibleMemberAs<std::string>("LocalMqName", cfg, m_localMqName);
+      m_remoteMqName = jutils::getPossibleMemberAs<std::string>("RemoteMqName", cfg, m_remoteMqName);
 
       int timeout = -1;
       timeout = jutils::getPossibleMemberAs<int>("DefaultTimeout", cfg, timeout);
       if (timeout >= 0) {
-        defaultTimeout = timeout;
+        m_defaultTimeout = timeout;
       }
     }
     catch (std::logic_error &e) {
-      CATCH_EX("Cannot read configuration file: " << PAR(cfgFileName), std::logic_error, e);
+      CATCH_EX("Cannot JSON parse configuration file: " << PAR(m_cfgFileName), std::logic_error, e);
       TRC_DBG("Using default params");
     }
   }
   else {
     TRC_DBG("Using default params");
   }
+  
+  f.close();
+  
+  //we work with these params
+  TRC_DBG(PAR(remoteMqName) << PAR(localMqName) << PAR(m_defaultTimeout));
 
-  if (json) {
-    int timeout = parseCommand(command);
-    if (timeout >= 0) {
-      defaultTimeout = timeout;
-    }
-    TRC_DBG("New timeout: " << PAR(defaultTimeout));
-  }
+  //instantiate incomming message queue
+  m_msgQueue = ant_new TaskQueue<ustring>([&](ustring msg) {
+    handleMessageReceived(msg);
+  });
 
-  if (raw) {
-    int timeout = -1;
-    timeout = std::stoi(rawTimeout);
-    if (timeout >= 0) {
-      defaultTimeout = timeout;
-    }
-    TRC_DBG("New timeout: " << PAR(defaultTimeout));
-  }
+  //instantiate MQ channel
+  m_mqChannel = ant_new MqChannel(remoteMqName, localMqName, 1024);
 
-  TRC_DBG(PAR(remoteMqName) << PAR(localMqName));
+  //register message handler for messages received from MQ channel
+  m_mqChannel->registerReceiveFromHandler([&](const std::basic_string<unsigned char>& msg) -> int {
+    //msgs are pushed to the queue here
+    m_msgQueue->pushToQueue(msg);
+    return 0;
+  });
 
-  MqChannel* mqChannel = ant_new MqChannel(remoteMqName, localMqName, 1024);
+  TRC_LEAVE("");
+}
 
-  // received messages from IQRF channel are pushed to IQRF MessageQueueChannel
-  mqChannel->registerReceiveFromHandler([&](const std::basic_string<unsigned char>& msg) -> int {
-    return handleMessageFromMq(msg); });
-
-  bool run = true;
-
-  if (cmdl) {
-    std::cout << std::endl << "iqrfapp " << PAR(IQRFAPP_VERSION) << PAR(BUILD_TIMESTAMP) << std::endl;
-  }
-
-  while (run)
-  {
-    if (cmdl) {
-      std::cout << std::endl << ">> ";
-      getline(std::cin, command);
-    }
-
-    ustring msgu((unsigned char*)command.data(), command.size());
-
-    if(!readonly) {
-      try {
-        mqChannel->sendTo(msgu);
-        sentFlag = true;
-      }
-      catch (std::exception& e) {
-        TRC_DBG("SendTo failed: " << e.what());
-        std::cerr << "Send failure" << std::endl;
-      }
-    }
-    else {
-      std::cout << "Listening for asynchronous messages, timeout: " << defaultTimeout << std::endl;
-    }
-
-    if (defaultTimeout != 0) {
-      for (int i = 0; i < defaultTimeout; i++) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        // exit when we receive response
-        if (exitFlag) break;
-      }
-    }
-    // defaultTimeout == 0 means waiting until there is the response
-    else {
-      while (!exitFlag)
-        ;
-    }
-
-    if (!cmdl) {
-      break;
-    }
-  }
-
-//TODO solve blocking dtor on WIN
+Iqrfapp::~Iqrfapp()
+{
+  delete m_msgQueue;
+  //TODO solve blocking dtor on WIN
 #ifndef WIN
   delete mqChannel;
 #endif
+}
 
-  return 0;
+int Iqrfapp::run(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+
+  int retval = 0;
+
+  if (0 == params.size()) {
+    interactiveCmd();
+  }
+  else {
+    retval = runCmd(params);
+  }
+
+  TRC_LEAVE(PAR(retval));
+  return retval;
+}
+
+int Iqrfapp::runCmd(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+
+  int retval = 0;
+
+  if (0 == params.size()) {
+    retval = -1;
+  }
+  else {
+    const std::string& cmd = params[0];
+
+    try {
+      if (cmd == CMD_HELP) {
+        helpCmd();
+      }
+      else if (cmd == CMD_READO) {
+        readonlyCmd(params);
+      }
+      else if (cmd == CMD_READA) {
+        readallCmd(params);
+      }
+      else if (cmd == CMD_RAW) {
+        rawCmd(params);
+      }
+      else if (cmd == CMD_CONF) {
+        confCmd(params);
+      }
+      else if (cmd == CMD_EXIT) {
+        retval = -3;
+      }
+      else if (cmd == CMD_JSON) {
+        jsonCmd(params);
+      }
+      else {
+        jsonCmd(params);
+      }
+    }
+    catch (std::logic_error& e) {
+      std::cerr << e.what() << std::endl;
+      helpCmd();
+      retval = -1;
+    }
+  }
+  TRC_LEAVE(PAR(retval));
+  return retval;
+}
+
+void Iqrfapp::handleMessageReceived(const ustring& message)
+{
+  TRC_ENTER("");
+  TRC_DBG("Received from MQ: " << std::endl << FORM_HEX(message.data(), message.size()));
+
+  {
+    std::unique_lock<std::mutex> lck(m_mutexMessageReceived);
+    m_messageReceived = message;
+    m_flagMessageReceived = true;
+    m_conditionMessageReceived.notify_all();
+  }
+
+  TRC_LEAVE("");
+}
+
+bool Iqrfapp::waitForMessageReceived(int extraTime)
+{
+  TRC_ENTER("");
+
+  bool retval = false;
+
+  if (m_actualTimeout <= 0) {
+    std::unique_lock<std::mutex> lck(m_mutexMessageReceived);
+    //wait for message
+    m_conditionMessageReceived.wait(lck, [&] { return m_flagMessageReceived; }); //lock is released in wait
+    //lock is reacquired here
+    m_flagMessageReceived = false;
+    retval = true;
+  }
+  else {
+    std::unique_lock<std::mutex> lck(m_mutexMessageReceived);
+    //wait for message
+    //add extraTime to extend waiting time to allow daemon process timeout response (otherwise it end earlier)
+    if (m_conditionMessageReceived.wait_for(lck, std::chrono::milliseconds(m_actualTimeout + extraTime), [&] { return m_flagMessageReceived; })) {
+      //finished waiting
+      m_flagMessageReceived = false;
+      retval = true;
+    }
+    else {
+      //timeout
+      m_flagMessageReceived = false;
+      retval = false;
+    }
+  }
+
+  TRC_LEAVE(PAR(retval));
+  return retval;
+}
+
+void Iqrfapp::showWaitForMessageReceivedResult(bool res)
+{
+  if (res) {
+    std::string msg((char*)m_messageReceived.data(), m_messageReceived.size());
+    std::cout << "Received: " << msg << std::endl;
+  }
+  else {
+    std::cout << "Timeout" << std::endl;
+  }
+}
+
+void Iqrfapp::setTimeout(int timeout)
+{
+  TRC_ENTER("");
+  if (timeout >= 0) {
+    m_actualTimeout = timeout;
+    TRC_DBG("Set: " PAR(m_actualTimeout));
+  }
+  else {
+    m_actualTimeout = m_defaultTimeout;
+    TRC_WAR(PAR(timeout) << "is invalid keep " <<  PAR(m_defaultTimeout));
+
+  }
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::sendParamsAsMessage(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+  std::ostringstream os;
+  for (const auto & par : params)
+    os << par << " ";
+  
+  std::string msg = os.str();
+  ustring msgu((unsigned char*)msg.data(), msg.size());
+
+  try {
+    m_mqChannel->sendTo(msgu);
+  }
+  catch (std::exception& e) {
+    TRC_DBG("SendTo failed: " << e.what());
+    std::cerr << "Send failure: " << e.what() << std::endl;
+  }
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::interactiveCmd()
+{
+  TRC_ENTER("");
+  while (true)
+  {
+    std::string command;
+    std::cout << std::endl << ">> ";
+    getline(std::cin, command);
+
+    //parse command
+    std::vector<std::string> params;
+    std::istringstream is(command);
+    std::string par;
+    while (!is.eof()) {
+      is >> par;
+      params.push_back(par);
+    }
+
+    int retval = runCmd(params);
+    if (-3 == retval)
+      break;
+  }
+
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::helpCmd()
+{
+  std::cerr << "Usage" << std::endl;
+  std::cerr << "  iqrfapp" << std::endl;
+  std::cerr << "  iqrfapp help" << std::endl;
+  std::cerr << "  iqrfapp readonly [timeout <ValueInMs>]" << std::endl;
+  std::cerr << "  iqrfapp readall" << std::endl;
+  std::cerr << "  iqrfapp raw <DpaBytesSeparatedByDots> [timeout <ValueInMs>]" << std::endl;
+  std::cerr << "  iqrfapp conf operational|forwarding|service" << std::endl;
+  std::cerr << "  iqrfapp [json] <JsonDpaRequest>" << std::endl;
+  std::cerr << "Examples" << std::endl;
+  std::cerr << "  iqrfapp" << std::endl;
+  std::cerr << "  iqrfapp help" << std::endl;
+  std::cerr << "  iqrfapp raw 01.00.06.03.ff.ff timeout 1000" << std::endl;
+  std::cerr << "  iqrfapp conf operational" << std::endl;
+  std::cerr << "  iqrfapp \"{\\\"ctype\\\":\\\"dpa\\\",\\\"type\\\":\\\"raw\\\",\\\"timeout\\\":1000,\\\"request\\\":\\\"01.00.06.03.ff.ff\\\"}\"" << std::endl;
+  std::cerr << "Tips" << std::endl;
+  std::cerr << "  Adjust default timeout in iqrfapp.json to the needs of the application using iqrfapp tool or define timeout as cmd param." << std::endl;
+  std::cerr << "  Default timeout in iqrfapp.json is 5000 ms." << std::endl;
+}
+
+void Iqrfapp::readonlyCmd(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+
+  if (params.size() != 1 && params.size() != 3) {
+    throw std::logic_error("expected 1 or 3 parameters");
+  }
+
+  int timeout = m_defaultTimeout;
+
+  if (params.size() == 3) {
+    if (params[1] != "timeout") {
+      throw std::logic_error("expected 2nd parameter: timeout");
+    }
+
+    try {
+      timeout = std::stoi(params[2]);
+    }
+    catch (std::invalid_argument&) {
+      throw std::logic_error("expected 3rd parameter <millis> representing integer value");
+    }
+  }
+
+  setTimeout(timeout);
+  std::cout << "Listening for asynchronous messages: " << NAME_PAR(timeout, m_actualTimeout) << std::endl;
+  bool res = waitForMessageReceived();
+  showWaitForMessageReceivedResult(res);
+
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::readallCmd(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+
+  if (params.size() != 1) {
+    throw std::logic_error("expected 1 parameters");
+  }
+
+  //TODO break by CTRL-C
+  while (true) {
+    std::cout << "Listening for asynchronous messages (stop by CTRL-C): " << std::endl;
+    setTimeout(0);
+    bool res = waitForMessageReceived();
+    showWaitForMessageReceivedResult(res);
+  }
+
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::rawCmd(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+  if (params.size() != 2 && params.size() != 4) {
+    throw std::logic_error("expected 2 or 4 parameters");
+  }
+
+  int timeout = m_defaultTimeout;
+
+  if (params.size() == 4) {
+    if (params[2] != "timeout") {
+      throw std::logic_error("expected 3rd parameter: timeout");
+    }
+
+    try {
+      timeout = std::stoi(params[3]);
+    }
+    catch (std::invalid_argument&) {
+      throw std::logic_error("expected 4th parameter <millis> representing integer value");
+    }
+  }
+
+  setTimeout(timeout);
+  sendParamsAsMessage(params);
+
+  bool res = waitForMessageReceived(500);
+  showWaitForMessageReceivedResult(res);
+
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::confCmd(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+  if (params.size() != 2) {
+    throw std::logic_error("expected 2 parameters");
+  }
+
+  const std::string par2 = params[1];
+  if (par2 != "operational" && par2 != "forwarding" && par2 != "service") {
+    throw std::logic_error("expected 3rd parameter: [operational | forwarding | service]");
+  }
+  else {
+  }
+
+  sendParamsAsMessage(params);
+
+  bool res = waitForMessageReceived(500);
+  showWaitForMessageReceivedResult(res);
+
+  TRC_LEAVE("");
+}
+
+void Iqrfapp::jsonCmd(const std::vector<std::string>& params)
+{
+  TRC_ENTER("");
+  std::string json;
+  if (params.size() == 2) {
+    json = params[1]; //possible format: json <JSON>
+  }
+  else if (params.size() == 1) {
+    json = params[0]; //possible format: <JSON>
+  }
+  else {
+    throw std::logic_error("expected 1 or 2 parameters");
+  }
+
+  //parse JSON
+  rapidjson::Document doc;
+  int timeout = m_defaultTimeout;
+
+  try {
+    jutils::parseString(json, doc);
+    jutils::assertIsObject("", doc);
+    timeout = jutils::getPossibleMemberAs<int>("timeout", doc, timeout); //optional
+  }
+  catch (std::exception &e) {
+    std::string errstr = "expected JSON format: ";
+    errstr += e.what();
+    throw std::logic_error(errstr);
+  }
+
+  setTimeout(timeout);
+  sendParamsAsMessage(params);
+
+  bool res = waitForMessageReceived(500);
+  showWaitForMessageReceivedResult(res);
+
+  TRC_LEAVE("");
 }
