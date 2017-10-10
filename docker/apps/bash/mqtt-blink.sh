be61617c99bc4b732969f9d1e1382c03ca48d31b
Rostislav Spinar <rostislav.spinar@microrisc.com>
2017-10-10 22:36:00 +0200

    Add docker sample apps

---
+++ docker/apps/bash/mqtt-blink.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Script for testing MQTT channel on Linux machine
+# mosquitto_pub is using MQTT local broker to communicate to IQRF daemon
+# Tested on Raspberry PI 3, Raspbian Lite
+# Tested on AAEON UP, UbiLinux
+
+# node address
+if [ ! -z $1 ]
+then
+# user address
+        node=$1
+else
+# auto address
+        node=01
+fi
+
+# sleep
+if [ ! -z $2 ]
+then
+# user time
+        time=$2
+else
+# auto time
+        time=10
+fi
+
+# packet counter
+cnt=1
+
+# end loop
+while [ true ]
+do
+	# echo
+	echo "sending request $cnt to pulse red led on node $node"
+
+	mosquitto_pub -h 10.1.1.1  -t "Iqrf/DpaRequest" -m "{\"ctype\":\"dpa\",\"type\":\"raw\",\"msgid\":\"$cnt\",\"request\":\"$node.00.06.03.ff.ff\",\"request_ts\":\"\",\"confirmation\":\".\",\"confirmation_ts\":\"\",\"response\":\".\",\"response_ts\":\"\"}"
+	
+	# counter
+	cnt=$((cnt+1))
+	
+	# sleep
+	sleep $time
+done
