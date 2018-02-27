e6e1f7d44af46f477c199862e97ec723c5394f26
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-02-27 15:25:38 +0100

    Adding script for renaming log file

---
+++ daemon/iqrf_startup/service/tracefile_add_timestamp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Log file renaming
+
+DAEMON_LOG_FILE=/var/log/iqrf-daemon.log
+TIMESTAMP=$(date "+%Y.%m.%d-%H.%M.%S")
+
+if [ -f "${DAEMON_LOG_FILE}" ]; then
+	echo "IQRF daemon log file renaming ..."
+	mv ${DAEMON_LOG_FILE} ${DAEMON_LOG_FILE}.${TIMESTAMP}.log
+else
+	echo "IQRF daemon log file does not exist!"
+fi
